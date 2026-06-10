7238f746dbffa80c39cbbbd10b80c506077a0fa6
Thomas <evers.thomas@gmail.com>
2026-06-10 19:22:00 +0200

    Entfernung von OG durch das Plugin

--- bootstrap/load.php
+++ bootstrap/load.php
@@ -122,9 +122,6 @@ if (file_exists(BES_DIR . 'frontend/includes/design-bridge.php')) {
 if (file_exists(BES_DIR . 'frontend/includes/schema.php')) {
     require_once BES_DIR . 'frontend/includes/schema.php';
 }
-if (file_exists(BES_DIR . 'frontend/includes/og-meta.php')) {
-    require_once BES_DIR . 'frontend/includes/og-meta.php';
-}
 if (file_exists(BES_DIR . 'frontend/includes/geo-utils.php')) {
     require_once BES_DIR . 'frontend/includes/geo-utils.php';
 }
